4b67e89b6c8761c3f2e76ac6f0ffb7b54546b02d
pasthortown <31218717+pasthortown@users.noreply.github.com>
2023-05-15 22:21:02 -0500

    ip

--- dockerimage/nettools.py
+++ dockerimage/nettools.py
@@ -8,6 +8,7 @@ import subprocess
 from pymongo import MongoClient
 import json
 from bson import json_util
+import socket
 
 mongo_bdd = os.getenv('mongo_bdd')
 mongo_bdd_server = os.getenv('mongo_bdd_server')
@@ -49,7 +50,8 @@ class ActionHandler(RequestHandler):
                 target = content['target']
                 respuesta = do_ping(target)
             if (action == 'get_pings'):
-                respuesta = get_pings()
+                target = content['target']
+                respuesta = get_pings(target)
             if (action == 'tracert'):
                 target = content['target']
                 respuesta = tracert(target)
@@ -58,14 +60,16 @@ class ActionHandler(RequestHandler):
         self.write(respuesta)
         return
 
-def get_pings():
+def get_pings(target):
     collection = db['pings']
-    toReturn = json.loads(json_util.dumps(collection.find({})))
+    toReturn = json.loads(json_util.dumps(collection.find({'target':target})))
     return {'response':toReturn, 'status':200}
 
 def do_ping(target):
     collection = db['pings']
+    ip = ''
     try:
+        ip = socket.gethostbyname(target)
         response = ping(target, timeout=2)
         if response is None:
             response = False
@@ -73,7 +77,7 @@ def do_ping(target):
     except Exception as e:
         toReturn = "No se pudo hacer ping a " + str(target)
         write_log(str(e))
-    collection.insert_one({'target':target, 'result':toReturn})
+    collection.insert_one({'target':target, 'ip': ip, 'result':toReturn})
     return {'response':toReturn, 'status':200}
 
 def tracert(target):
