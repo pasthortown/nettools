de32d4174ad3b01474b69bce0a7e97c0c434d72c
Luis Alfonso Salazar Vaca <luis.salazar@kfc.com.ec>
2023-05-18 11:48:20 -0500

    builded

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  api_entities: 'http://localhost:9896/',
-  api_foreground: 'http://localhost:9898/',
+  api_foreground: 'http://localhost:9890/',
+  api_entities: 'http://localhost:9891/',
   token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfbmFtZSI6Ik5ldFRvb2xzIiwidmFsaWRfdW50aWwiOiIyMDMzLTA1LTEzIDA5OjI4OjI2LjE3NzQ5NCJ9.KOOApegGeEH-l8ZQBXt5QC-cV3CsRb0czvgOupSdltk'
 };
