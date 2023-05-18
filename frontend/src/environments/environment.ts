4b7dc2599374562b41ab5365baac6320a59d6de8
Luis Alfonso Salazar Vaca <luis.salazar@kfc.com.ec>
2023-05-18 11:55:24 -0500

    correccion ip

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  api_foreground: 'http://localhost:9890/',
-  api_entities: 'http://localhost:9891/',
+  api_foreground: 'http://192.168.101.39:9890/',
+  api_entities: 'http://192.168.101.39:9891/',
   token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfbmFtZSI6Ik5ldFRvb2xzIiwidmFsaWRfdW50aWwiOiIyMDMzLTA1LTEzIDA5OjI4OjI2LjE3NzQ5NCJ9.KOOApegGeEH-l8ZQBXt5QC-cV3CsRb0czvgOupSdltk'
 };
